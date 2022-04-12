4d1110c43ee79c31b819a0941d911a5eba41d0be
Josh Babin <josh.babin@gmail.com>
2022-04-12 02:27:09 -0500

    Converting everything into functions, within Tkinter GUI. Dataframe records are not storing/persisting.

--- ToDo.py
+++ ToDo.py
@@ -1,22 +1,98 @@
 # Import libraries
 from os import *
+from tkinter import *
 import pandas as pd
 import numpy as np
 
+#Instantiate tkinter window
+root = Tk()
+root.title("To-Do List")
+
+#Define Variables
 fname = 'ToDoList.csv'
-flag = None
+task_val = StringVar()
+due_val = StringVar()
+priority_val = StringVar()
 
 # If there is an existing file, open it to dataframe. If not, create an empty dataframe to save to file later
 try:
-    df = pd.read_csv(fname, sep=',', index_col=0)
+    data = pd.read_csv(fname, sep=',', index_col=0)
 except:
     columns = ['Task','Due Date','Priority']
-    df = pd.DataFrame(columns=columns)
+    data = pd.DataFrame(columns=columns)
 
-# Print out the current task List
-print('Current Task List:\n', df)
+#Define functions
 
-# Run loop until Q is pressed
+def print_tasks():
+    df=data.get()
+    list_label = Label(root, text=df)
+    list_label.grid(row=5, column=0, columnspan=2)
+
+def add_task():
+    task_val = taskE.get()
+    due_val = dueDateE.get()
+    priority_val = priorityE.get()
+    df=data.get()
+    newrow = {'Task' : [task_val],
+            'Due Date' : [due_val],
+            'Priority' : [priority_val]}
+    df2 = pd.DataFrame(newrow)
+    data = pd.concat([df, df2], axis=0).reset_index(drop=True)
+    print(data)
+    return data
+
+def del_task():
+    df=data.get()
+    recordid = int(taskidE.get())
+    data = df.drop(recordid).reset_index(drop=True)
+    return data
+
+def save(df):
+    df.to_csv(fname, mode='w')
+
+#Create field labels/titles
+task = Label(root, text="Task Name: ")
+task.grid(row=0,column=0)
+due = Label(root, text="Due Date: ")
+due.grid(row=1, column=0)
+priority = Label(root, text="Priority")
+priority.grid(row=2, column=0)
+taskid = Label(root, text="Task ID")
+taskid.grid(row=3, column=0)
+
+#Create User Input Fields
+taskE = Entry(root, width=35, border=5)
+taskE.grid(row=0, column=1)
+dueDateE = Entry(root, width=35, border=5)
+dueDateE.grid(row=1, column=1)
+priorityE = Entry(root, width=35, border=5)
+priorityE.grid(row=2, column=1)
+taskidE = Entry(root, width=35, border=5)
+taskidE.grid(row=3, column=1)
+
+# # # Note to self - stack the labels and the entry fields together into one code chunk
+
+#Define buttons
+
+printButton = Button(root, text="Print Tasks", padx=40, pady=20, command=print_tasks)
+addButton = Button(root, text="Add Task", padx=40, pady=20, command=add_task())
+deleteButton = Button(root, text="Delete Task", padx=40, pady=20, command=del_task)
+saveButton = Button(root, text="Save List", padx=40, pady=20, command= save(data))
+quitButton = Button(root, text="Quit", padx=40, pady=20, command=root.quit)
+
+
+#Put buttons on the screen
+printButton.grid(row=0, column=2)
+addButton.grid(row=1, column=2)
+deleteButton.grid(row=2, column=2)
+saveButton.grid(row=3, column=2)
+quitButton.grid(row=4, column=2)
+
+root.mainloop()
+
+
+
+""" # Run loop until Q is pressed
 
 while flag != 'Q':
     option = input("Options: A to Add, D to Delete, P to Print Full Task List, Q to Quit \nAction: ")
@@ -46,4 +122,4 @@ while flag != 'Q':
         print("Closing.")
         exit
     else:
-        print("Not a valid option. Please try again.")
+        print("Not a valid option. Please try again.") """
